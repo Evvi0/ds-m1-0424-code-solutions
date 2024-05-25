e7b9e154dc7c4243b5f3eb950b7e3f9c4c5ba0c2
Evvi0 <eviosofario@gmail.com>
2024-05-25 19:11:27 -0400

    Fixed Sql additions

--- sql-common-clauses/Sql-common-clauses.sql
+++ sql-common-clauses/Sql-common-clauses.sql
@@ -3,14 +3,16 @@ SELECT id, log_feature log, volume vol FROM log_feature lf
 SELECT id, resource_type 
 FROM resource_type rt 
 ORDER BY id ASC
+LIMIT 5
 
 SELECT id, resource_type 
 FROM resource_type rt 
 ORDER BY id DESC
+LIMIT 5
 
 SELECT id, resource_type 
 FROM resource_type rt 
-ORDER BY id DESC
+ORDER BY id ASC, resource_type DESC
 LIMIT 5
 
 SELECT COUNT(*), COUNT(DISTINCT id) id_unique, COUNT(DISTINCT severity_type) severity_type_nunique
@@ -18,4 +20,5 @@ FROM severity_type st
 
 SELECT id, log_feature, volume FROM log_feature lf 
 WHERE volume >= 100 AND volume <= 300 AND log_feature = 'feature 201'
+ORDER BY volume
 
